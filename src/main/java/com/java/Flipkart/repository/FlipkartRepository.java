b05cfd13752ffd975fea6201d52503ab3b35b174
jitenahuja6 <38278166+jitenahuja6@users.noreply.github.com>
2024-10-06 16:02:03 +0530

    "RepositoryCommit"

--- src/main/java/com/java/Flipkart/repository/FlipkartRepository.java
+++ src/main/java/com/java/Flipkart/repository/FlipkartRepository.java
@@ -4,7 +4,7 @@ import com.java.Flipkart.model.Flipkart;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.List;
-
+//Repository Test
 public interface FlipkartRepository extends MongoRepository<Flipkart, String> {
 
     List<Flipkart> getByPrice(int price);
